c98f42a1219a7af22f946fca2824a4c43c774324
Ivan Kliachenko <ivan.green69@gmail.com>
2023-12-28 19:05:02 +0200

    task/022 list solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,54 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        with(binding) {
+            actionButton.setOnClickListener {
+                val inputText = inputEditText.text
+                addTextView(inputText.toString())
+                inputEditText.text?.clear()
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val textViewTextList = binding.contentLayout.children.map {
+            (it as TextView).text.toString()
+        }.toList()
+        outState.putStringArrayList(KEY, ArrayList(textViewTextList))
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val textViewList = savedInstanceState.getStringArrayList(KEY) ?: ArrayList()
+        textViewList.forEach {
+            addTextView(it.toString())
+        }
+    }
+
+    companion object {
+        private const val KEY = "textKey"
+    }
+
+    private fun addTextView(inputText: String) {
+        val textView = TextView(this@MainActivity).apply {
+            text = inputText
+        }
+        binding.contentLayout.addView(textView)
     }
 }
